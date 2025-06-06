fff5a80d7bebcc80e2cd9d3302440a2d948d5050
Daniel Figueredo <danielfsa2007@gmail.com>
2025-06-06 19:02:05 -0300

    aprendendo a usar condições
    aprendi a usar condições (if e else) e pratiquei fazendo um identificador de paises como o professor pediu

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,7 @@
-console.log("bla")
+var vel = 70.5
+console.log(`A velocidade do seu carro é igual a ${vel}Km/h`)
+if (vel > 60){
+    console.log(`Voce ultrapassou o limite de velocidade`)
+} else {
+    console.log(`Dirija sempre com cuidado`)
+}
