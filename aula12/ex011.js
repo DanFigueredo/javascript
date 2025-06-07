40b46258ec1f9efce30d44ffa04a91013ef479e2
Daniel Figueredo <danielfsa2007@gmail.com>
2025-06-07 17:04:42 -0300

    Aprendendo mais sobre condições
    Aprendi a usar o switch em JS e treinei mais sobre condições

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade  = 65
+console.log(`Vc tem ${idade} anos`)
+if (idade < 16){
+
+    console.log("Não vota")
+} else if ( idade < 18 || idade >= 65){
+    console.log("voto opcional")
+} else{
+    console.log("Voto obrigatorio")
+}
