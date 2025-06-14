8e5664d1b6c40357207c82642d56c59bc31b8e24
Daniel Figueredo <danielfsa2007@gmail.com>
2025-06-13 21:59:40 -0300

    .

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,11 +4,13 @@ function contar(){
     var n1 = Number(document.getElementById("n1").value)
     var n2 = Number(document.getElementById("n2").value)
     var passo = Number (document.getElementById("passo").value)
-    var res = document.getElementById("res")
+    var res = document.getElementById("res")  
 
-    while(n1<= n2){
-        
-     res.innerHTML =  `${n1}`
+    while(n1 <= n2){
+    
+   
+
+     res.innerHTML +=  `${n1}`
       
     }
 }
