8312434ec8cba4d13cfb9e34fe07d579c818ef26
Daniel Figueredo <danielfsa2007@gmail.com>
2025-06-15 22:14:46 -0300

    finalizando o ex 016
    utilizei o if, else junto com o while para executar o que foi pedido

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,13 +4,22 @@ function contar(){
     var n1 = Number(document.getElementById("n1").value)
     var n2 = Number(document.getElementById("n2").value)
     var passo = Number (document.getElementById("passo").value)
-    var res = document.getElementById("res")  
-
-    while(n1 <= n2){
-    
-   
-
-     res.innerHTML +=  `${n1}`
-      
+    var res = document.getElementById("res") 
+     
+    if(passo <= 0){
+        alert("O passo 0 não pode ser utilizado, considerando 1")
+        passo = 1
     }
+    if (n1 < n2){
+        while(n1 <= n2){
+            res.innerHTML += `${n1}`
+            n1+=passo
+        }
+    } else{
+    while(n1 >= n2)
+        res.innerHTML += `${n1}`
+        n1-=passo
+    }
+
+    
 }
