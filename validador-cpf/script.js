f0381f0c60a916878ab1d7827b9273b0159a2535
Daniel Figueredo <danielfsa2007@gmail.com>
2025-06-30 15:45:21 -0300

    Praticando js
    tentei fazer um validador de cpf manipulando os elementos e dizendo se o cpf é valido ou não

---
+++ validador-cpf/script.js
@@ -0,0 +1,23 @@
+
+
+
+function validar(){
+    
+    let cpf = document.getElementById("input-cpf").value
+    cpf = cpf.replace(/\D/g, '')
+    let res = document.getElementById('res')
+    
+    if(cpf === ''){
+        res.innerHTML =("Digite um CPF Valido!")
+        return;
+    }
+    if(cpf.length !== 11){
+        res.innerHTML =("Digite um CPF Valido!")
+        return;
+    }
+
+    res.innerHTML = ('Seu CPF foi validado com sucesso!')
+     document.getElementById("input-cpf").value = ''
+     document.getElementById("input-cpf").focus()
+    
+}
