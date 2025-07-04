984d109d2a29c8b230ab002c12794b899db304cc
Daniel Figueredo <danielfsa2007@gmail.com>
2025-07-04 19:53:56 -0300

    Update script.js

--- projeto/script.js
+++ projeto/script.js
@@ -1,14 +1,17 @@
+//função pra quando clicar no botão de se inscrever
 function subscribeButton(){
+    //pegando os valores e fazendo as variaveis 
     let email = document.getElementById("input-email")
     let res = document.querySelector("#res")
+    //Caso o usuario não tenha digitado nada no input
     if(email.value == ""){
-        res.style.color = "red"
-        res.innerHTML = "Valid email required!"
-        email.style.border = "1px solid red";
+        res.style.color = "red" //cor do texto vermelho
+        res.innerHTML = "Valid email required!" //Um aviso para o usuario digitar um email valido
+        email.style.border = "1px solid red"; //bordas ficam vermelhas pra sinalizar negativo
         email.style.color = "red"
-        return
+        return // Para a função acabar aqui caso o if esteja true 
             
     }
-    location.href = "http://127.0.0.1:5501/projeto/pg02.html"
+    location.href = "/projeto/pg02.html" // Quando clicar no botão o usuario é levado em outra pagina
 }
 
